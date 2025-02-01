52a82fc181603ea7fd1f53108ef1e0799a44f9db
Luis Arthur <luis-arthurabelo@hotmail.com>
2025-02-01 15:50:40 -0300

    bikerman update

--- Assets/ProgrammingUI/Scripts/bikeman/BikermanControl.cs
+++ Assets/ProgrammingUI/Scripts/bikeman/BikermanControl.cs
@@ -33,6 +33,7 @@ public class BikermanControl : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
         StartCoroutine(callProvocationsLines());
+        //StartCoroutine(ChangeDirectionRoutine());
     }
 
     private void Update()
@@ -44,6 +45,17 @@ public class BikermanControl : MonoBehaviour
             movementSpeed = 80;
         }
 
+    }
+
+    private IEnumerator ChangeDirectionRoutine()
+    {
+        while (true)
+        {
+            float randomAngle = Random.Range(-20f, 20f); // Adjust the range as needed
+            transform.forward = Quaternion.Euler(0, randomAngle, 0) * transform.forward;
+
+            yield return new WaitForSeconds(Random.Range(3, 7)); // Change direction every 3-7 seconds
+        }
     }
 
     private IEnumerator callProvocationsLines()
